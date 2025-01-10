2c179a8c456b162b21e7cbab6cf6088866c60c7e
Emmanuel Monle <emmanuel.monle@gmail.com>
2025-01-10 15:13:14 +0200

    start homepage design

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.tsx'
+import Station from './pages/station.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/stations/:name" element={<Station />} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
